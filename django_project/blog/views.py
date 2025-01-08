14a95afe59d4099af23d725fe5618457c67451f6
martand-aditya <martand.aditya@gmail.com>
2025-01-09 00:00:53 +0530

    Adding home page and about page for blog

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#used when we have to return httpresponse
+#from django.http import HttpResponse
 
 # Create your views here.
+#list of dictionaries
+posts = [
+    {
+        'author': 'martand',
+        'title': 'Blog Post 1',
+        'content': 'This is first blog post and creating content for display',
+        'date_posted':'January 8 2025'
+    },
+    {
+        'author': 'aditya',
+        'title': 'Blog Post 2',
+        'content': 'This is second blog post and testing the looping over data in template',
+        'date_posted':'January 9 2025'
+    },
+]
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+
+        'posts':posts
+    }
+    #return HttpResponse('<h1>Blog home</h1>')
+    #render inbuilt function which returns rendered html page
+    #passing context as an argument for html html will have access to variable 'posts' directly
+    return render(request, 'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    #return HttpResponse('<h1>Blog About</h1>')
+    #return render(request,'blog/about.html')
+    return render(request,'blog/about.html',{'title':'About'})
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #added app blogs app config to look for templates
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
